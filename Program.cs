4e1605b10c149e75f81af234eef6f195d28582db
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-16 17:21:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int constantNumber = 7;
+            int initialNumber = 5;
+
+            for (int i = 0; i < 14; i++) //Выбрал именно этот цикл, потому что точно знаю сколько итераций он должен сделать
+            {
+                Console.WriteLine(initialNumber);
+                int Intermediate = initialNumber + constantNumber;
+                initialNumber = Intermediate;
+            }
+            Console.ReadLine();
+        }
+    }
+}
