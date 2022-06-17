0c76967f559d6cae49d7e8d381619c9433b8e7f3
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-17 15:13:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int min = 1;
+            int max = 101;
+            int randomNumber = rand.Next(min, max);
+            int number = 2;
+
+            for (int i = number; i <= randomNumber; i *= 2)
+            {
+                number = i;
+                Console.WriteLine($"Начально число: {randomNumber}. Возводимое в степень число:{number}.");
+            }
+            Console.WriteLine($"Начально число: {randomNumber}. Возводимое в степень число:{number * 2}.");
+            Console.ReadLine();
+        }
+    }
+}
