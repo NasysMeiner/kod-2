078b510adfd01b4d02733fc6bd82168170c1fb63
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-22 18:36:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            bool success = false;
+
+            while (isWork)
+            {
+                int number = Convert(ref success);
+
+                if (success)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Вы ввели {number}");
+                    isWork = false;
+                }                
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Convert(ref bool success)
+        {
+            Console.WriteLine("Введите число:");
+            string userInput = Console.ReadLine();
+            bool result = int.TryParse(userInput, out int number);
+            
+            if (result)
+            {
+                success = true;
+                return number;
+            }
+
+            return 0;
+        }
+    }
+}
