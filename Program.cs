a825012ba4ebab32da76dc02f5a670b0d756cebb
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-07-04 01:07:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,70 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace hw7._2
+namespace hw7._3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Database database = new Database();
-            database.Work();
+            Hospital hospital = new Hospital();
+            hospital.Work();
         }
     }
 
-    class Database
+    class Hospital
     {
-        private List<Criminal> _criminals = new List<Criminal>();
+        private List<Patient> _patients = new List<Patient>();
 
-        public Database()
+        public Hospital()
         {
-            _criminals.Add(new Criminal("Oleg", "Антиправительственное"));
-            _criminals.Add(new Criminal("Андрей", "Неизвестно"));
-            _criminals.Add(new Criminal("Эдик", "Антиправительственное"));
-            _criminals.Add(new Criminal("Владик", "Неизвестно"));
-            _criminals.Add(new Criminal("Никита", "Антиправительственное"));
-            _criminals.Add(new Criminal("Акакий", "Антиправительственное"));
-            _criminals.Add(new Criminal("Валерий", "Неизвестно"));
-            _criminals.Add(new Criminal("Глеб", "Неизвестно"));
-            _criminals.Add(new Criminal("Марк", "Антиправительственное"));
-            _criminals.Add(new Criminal("Карл", "Антиправительственное"));
+            _patients.Add(new Patient("Oleg", "ОРВИ", 18));
+            _patients.Add(new Patient("Андрей", "Грип", 16));
+            _patients.Add(new Patient("Эдик", "ОРВИ", 30));
+            _patients.Add(new Patient("Владик", "Грип", 57));
+            _patients.Add(new Patient("Никита", "ОРВИ", 24));
+            _patients.Add(new Patient("Акакий", "ОРВИ", 32));
+            _patients.Add(new Patient("Валерий", "Ветрянка", 26));
+            _patients.Add(new Patient("Глеб", "Грип", 38));
+            _patients.Add(new Patient("Марк", "Ветрянка", 17));
+            _patients.Add(new Patient("Карл", "Грип", 27));
         }
 
         public void Work()
         {
-            ShowInfo();
-            Console.ReadLine();
-            Console.Clear();
+            string userInput;
 
-            Console.WriteLine("Амнистия.");
-            Console.ReadLine();
-            Console.Clear();
+            while (true)
+            {
+                ShowInfo();
+                Console.WriteLine("Меню\n1 - отсортировать список по ФИО.\n2 - отсортировать по возрасту\n3 - вывести больных с кокретным заболеванием.");
+                userInput = Console.ReadLine();
+                Console.Clear();
 
-            var criminalsSort = _criminals.Where(criminal => criminal.Cause != "Антиправительственное");
-            _criminals = criminalsSort.ToList();
-            ShowInfo();
-            Console.ReadLine();
+                if(int.TryParse(userInput, out int input))
+
+                switch (input)
+                {
+                    case 1:
+                        SortName();
+                        break;
+                    case 2:
+                        SortAge();
+                        break;
+                    case 3:
+                        ShowDisease();
+                        break;
+                }                   
+            }
         }
 
         public void ShowInfo()
         {
-            foreach(var criminal in _criminals)
+            int positionY = 0;
+
+            foreach(Patient patient in _patients)
             {
-                Console.WriteLine($"{criminal.Name} - {criminal.Cause}.");
+                Console.SetCursorPosition(80, positionY);
+                Console.WriteLine($"{patient.Name} - {patient.Age} лет, болен:{patient.Disease}.");
+                positionY++;
             }
+        }
+
+        private void SortName()
+        {
+            var sortName = _patients.OrderBy(patient => patient.Name.Substring(0, 1));
+            _patients = sortName.ToList();
+            Console.WriteLine("Сортировка выполнена");
+        }
+
+        private void SortAge()
+        {
+            var sortAge = _patients.OrderBy(patient => patient.Age);
+            _patients = sortAge.ToList();
+            Console.WriteLine("Сортировка выполнена");
+        }
+
+        private void ShowDisease()
+        {
+            string userInputDisease;
+
+            Console.Write("Введите болезнь:");
+            userInputDisease = Console.ReadLine();
+
+            _patients.Sort((left, right) => left.Disease.CompareTo(right.Disease));
+            var diseases = _patients.Where(patient => patient.Disease.StartsWith(userInputDisease));
+
+            Console.WriteLine();
+            
+            foreach (var disease in diseases)
+            {
+                Console.WriteLine($"{disease.Name} - {disease.Age} лет, болен:{disease.Disease}.");
+            }           
         }
     }
 
-    class Criminal
+    class Patient
     {
         public string Name { get; private set; }
-        public string Cause { get; private set; }
+        public string Disease { get; private set; }
+        public int Age { get; private set; }
 
-        public Criminal(string name, string cause)
+        public Patient(string name, string disease, int age)
         {
             Name = name;
-            Cause = cause;
+            Disease = disease;
+            Age = age;
         }
     }
 }
